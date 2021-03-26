33cb9ddc0a7ed1d3f69bfaf297e4cb7e5297d285
amsen20 <amsen@aut.ac.ir>
2021-03-26 19:32:33 +0430

    Add and, or, not, cmp and cond commands.

--- engine/commands.h
+++ engine/commands.h
@@ -3,11 +3,18 @@
 
 #include "prestate.h"
 
-// commands:
+// operations:
 #define ADD "+"
 #define SUB "-"
 #define MUL "*"
-#define CON ">-"
+#define AND_n "&"
+#define OR_n "|"
+#define NOT_n "!"
+#define CMP ">"
+
+// commands:
+#define CONN ">-"
+#define COND "?"
 
 // keywords
 #define DEF "def"
@@ -20,7 +27,7 @@
 #define IN_PIN "input-pin"
 #define OUT_PIN "output-pin"
 
-Box *add, *sub, *mul, *con, *in_pin, *out_pin;
+Box *add, *sub, *mul, *and_n, *or_n, *not_n, *cmp, *conn, *in_pin, *out_pin, *cond;
 
 void set_up(std::map<std::string, Box*> &types){
     add = new Box(true, ADD, {"a", "b"}, {"c"});
@@ -41,13 +48,48 @@ void set_up(std::map<std::string, Box*> &types){
         return {vec[0] * vec[1]};
     };
 
-    con = new Box(true, CON, {"a", "b"}, {"c"});
-    types[con->name] = con;
-    con->func = [](std::vector<Value> vec) -> std::vector<Value>{
+    and_n = new Box(true, AND_n, {"a", "b"}, {"c"});
+    types[and_n->name] = and_n;
+    and_n->func = [](std::vector<Value> vec) -> std::vector<Value>{
+        return {vec[0] & vec[1]};
+    };
+
+    or_n = new Box(true, OR_n, {"a", "b"}, {"c"});
+    types[or_n->name] = or_n;
+    or_n->func = [](std::vector<Value> vec) -> std::vector<Value>{
+        return {vec[0] | vec[1]};
+    };
+
+    not_n = new Box(true, NOT_n, {"a"}, {"c"});
+    types[not_n->name] = not_n;
+    not_n->func = [](std::vector<Value> vec) -> std::vector<Value>{
+        return {!vec[0]};
+    };
+
+    cmp = new Box(true, CMP, {"a", "b"}, {"c"});
+    types[cmp->name] = cmp;
+    cmp->func = [](std::vector<Value> vec) -> std::vector<Value>{
+        return {vec[0] > vec[1]};
+    };
+
+    conn = new Box(true, CONN, {"a", "b"}, {"c"});
+    conn->sync = false;
+    types[conn->name] = conn;
+    conn->func = [](std::vector<Value> vec) -> std::vector<Value>{
         if(vec[0] != -1)
             return {vec[0]};
         else
             return {vec[1]};
+    };
+
+    cond = new Box(true, COND, {"a", "b"}, {"c"});
+    cond->controller = true;
+    types[cond->name] = cond;
+    cond->check = [](std::vector<Value> vec) -> bool{
+        return vec[1];
+    };
+    cond->func = [](std::vector<Value> vec) -> std::vector<Value>{
+        return {vec[0]};
     };
 
     in_pin = new Box(true, IN_PIN, {}, {"a"});
